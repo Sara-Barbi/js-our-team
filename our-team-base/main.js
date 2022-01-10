aa17ca9cb62e4dbcde2e0398f396ef38b59571e9
Sara-Barbi <barbisara02@gmail.com>
2022-01-10 18:42:17 +0100

    inizio bonus

--- our-team-base/main.js
+++ our-team-base/main.js
@@ -38,46 +38,10 @@ let add= document.getElementById("addMemberButton");
 add.addEventListener('click', function(){
 
    
-
-
-  let ilNome =document.getElementById("name").value;
-  let ilRuolo =document.getElementById("role").value;
-  let lImg =document.getElementById("image").value;
-  
-  let divCard = document.createElement("div");
-  divCard.classList.add("card-text");
-
-  let divImg = document.createElement("div");
-  divImg.classList.add("card-image");
-
-  let pName = document.createElement("h3");
-  pName.textContent = ilNome;
-  divCard.appendChild(pName);
-
-  let pRole = document.createElement("p");
-  pRole.textContent = ilRuolo;
-  divCard.appendChild(pRole);
-  
-  let pImg = document.createElement("img");
-  pImg.src = lImg;
-  pImg.alt = ilNome;
-  divImg.appendChild(pImg);
-
-  let element = document.getElementsByClassName("team-card");
-  element.appendChild(divImg);
-  element.appendChild(divCard);
-
-  let container = document.getElementById("team-container");
-  container.appendChild(element);
 })
 */
 
 
-/*
-for(let i in team){
-  console.log(team[i]);
-}
-*/
 
 let teamContainer = document.querySelector(".team-container");
 
